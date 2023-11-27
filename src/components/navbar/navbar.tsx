abb2465d2ecec7f6df3f879898493dae3d0060c3
Tyler Randall <tyler7r@gmail.com>
2023-11-27 15:42:57 -0500

    Install material ui deps

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,17 +1,18 @@
-import { useAuthContext } from "~/contexts/auth";
 import { useMobileContext } from "~/contexts/mobile";
 import { Button } from "../button/button";
 import { Logo } from "../logo/logo";
 
 export const Navbar = () => {
   const isMobile = useMobileContext();
-  const auth = useAuthContext();
+  // const auth = useAuthContext();
 
   return isMobile ? (
-    <div className="m-0 p-0">
+    <div className="align-center flex justify-between">
       <Logo size="small" />
-      <Button label="Login" primary={false} size="small" />
-      <Button label="Signup" primary={true} size="small" />
+      <div className="align-center flex justify-center gap-2 px-1 py-4">
+        <Button label="Signup" primary={true} size="small" />
+        <Button label="Login" primary={false} size="small" />
+      </div>
     </div>
   ) : (
     <div className="m-0 p-0">
