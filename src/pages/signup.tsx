49194421e6482c0606f616b04bbaf98a49f8c86d
Tyler Randall <tyler7r@gmail.com>
2023-12-04 14:48:42 -0500

    Initial signup page styling

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -10,15 +10,16 @@ const Signup = () => {
   };
 
   return (
-    <div className="flex h-full w-full flex-col items-center justify-center text-center">
+    <div className="mt-10 flex w-full flex-col items-center justify-center gap-8 text-center">
       <Typography variant="h1" fontSize={72}>
         Sign Up
       </Typography>
       <form
         onSubmit={handleSubmit}
-        className="flex w-4/5 flex-col items-center justify-center text-center"
+        className="flex w-4/5 flex-col items-center justify-center gap-4 text-center"
       >
         <TextField
+          className="w-full"
           name="email"
           autoComplete="email"
           required
@@ -35,6 +36,12 @@ const Signup = () => {
           Signup
         </Button>
       </form>
+      <div className="mt-2 flex flex-col items-center justify-center gap-2">
+        <div className="">Already have an account?</div>
+        <Button variant="outlined" size="small">
+          Login
+        </Button>
+      </div>
     </div>
   );
 };
