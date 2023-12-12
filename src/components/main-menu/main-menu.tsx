9599319b87ae3749e9846165890381fa75878dd1
Tyler Randall <tyler7r@gmail.com>
2023-12-12 09:43:56 -0500

    Add size prop to main menu

--- src/components/main-menu/main-menu.tsx
+++ src/components/main-menu/main-menu.tsx
@@ -1,7 +1,11 @@
 import { Button, colors } from "@mui/material";
 import { useIsDarkContext } from "~/pages/_app";
 
-export const MobileMenu = () => {
+type MainMenuProps = {
+  size: "small" | "medium" | "large";
+};
+
+const MainMenu = ({ size }: MainMenuProps) => {
   const isDark = useIsDarkContext();
   return (
     <div
@@ -10,18 +14,20 @@ export const MobileMenu = () => {
       }}
       className={`flex items-center justify-between`}
     >
-      <Button variant="text" size="medium">
+      <Button variant="text" size={size}>
         Film Room
       </Button>
-      <Button variant="text" size="medium">
+      <Button variant="text" size={size}>
         Highlight Factory
       </Button>
-      <Button variant="text" size="medium">
+      <Button variant="text" size={size}>
         Team Profile
       </Button>
-      <Button variant="text" size="medium">
+      <Button variant="text" size={size}>
         Inbox
       </Button>
     </div>
   );
 };
+
+export default MainMenu;
--- src/components/navbar/desktop-nav.tsx
+++ src/components/navbar/desktop-nav.tsx
@@ -1,9 +1,10 @@
 import DarkModeIcon from "@mui/icons-material/DarkMode";
 import LightModeIcon from "@mui/icons-material/LightMode";
-import { Button, Switch, colors } from "@mui/material";
+import { Button, Switch } from "@mui/material";
 import { useIsDarkContext } from "~/pages/_app";
 // import { Button } from "../button/button";
 import { Logo } from "../logo/logo";
+import MainMenu from "../main-menu/main-menu";
 
 type DesktopNavProps = {
   switchTheme: () => void;
@@ -13,41 +14,30 @@ const DesktopNav = ({ switchTheme }: DesktopNavProps) => {
   const isDark = useIsDarkContext();
 
   return (
-    <div className="grid grid-cols-5 items-center gap-5 px-3">
-      <Logo size="large" />
-      <div
-        style={{
-          borderColor: `${isDark ? colors.grey[900] : colors.grey[100]}`,
-        }}
-        className="wrap col-span-3 flex flex-wrap items-center justify-around border-solid"
-      >
-        <Button variant="text" size="large" href="/film-room">
-          Film Room
-        </Button>
-        <Button variant="text" size="large">
-          Highlight Factory
-        </Button>
-        <Button variant="text" size="large">
-          Team Profile
-        </Button>
-        <Button variant="text" size="large">
-          Inbox
-        </Button>
+    <div className="">
+      <div className="grid grid-cols-5 items-center gap-5 px-3">
+        <Logo size="large" />
+        <div className="col-span-1 flex items-center gap-2">
+          <Button variant="contained" size="large" href="/signup">
+            Signup
+          </Button>
+          <Button
+            variant="outlined"
+            disabled={false}
+            size="large"
+            href="/login"
+          >
+            Login
+          </Button>
+          <Switch
+            className="items-center justify-center"
+            icon={<LightModeIcon color="primary" fontSize="small" />}
+            onChange={switchTheme}
+            checkedIcon={<DarkModeIcon fontSize="small" />}
+          />
+        </div>
       </div>
-      <div className="col-span-1 flex items-center gap-2">
-        <Button variant="contained" size="large" href="/signup">
-          Signup
-        </Button>
-        <Button variant="outlined" disabled={false} size="large" href="/login">
-          Login
-        </Button>
-        <Switch
-          className="items-center justify-center"
-          icon={<LightModeIcon color="primary" fontSize="small" />}
-          onChange={switchTheme}
-          checkedIcon={<DarkModeIcon fontSize="small" />}
-        />
-      </div>
+      <MainMenu size="large" />
     </div>
   );
 };
