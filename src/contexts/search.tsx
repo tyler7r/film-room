0e8a9a6f1fb64b08495ff5ed321f75d058b1365b
Tyler Randall <tyler7r@gmail.com>
2024-07-28 14:50:22 -0400

    Adjust context of global search

--- src/contexts/search.tsx
+++ src/contexts/search.tsx
@@ -5,28 +5,21 @@ type SearchProps = {
 };
 
 type SearchContextType = {
-  // isOpen: boolean;
-  // setIsOpen: (isOpen: boolean) => void;
   topic: string;
   setTopic: (topic: string) => void;
 };
 
 export const SearchContext = createContext<SearchContextType>({
-  // isOpen: false,
-  // setIsOpen: () => null,
   topic: "",
   setTopic: () => null,
 });
 
 export const GlobalSearch = ({ children }: SearchProps) => {
-  // const [isOpen, setIsOpen] = useState<boolean>(false);
   const [topic, setTopic] = useState<string>("");
 
   return (
     <SearchContext.Provider
       value={{
-        // isOpen,
-        // setIsOpen,
         topic,
         setTopic,
       }}
