af5068573c34240a7026474e5a877a856274c80a
Tyler Randall <tyler7r@gmail.com>
2024-08-20 22:08:39 -0400

    Minor typo fix

--- src/pages/team-select/index.tsx
+++ src/pages/team-select/index.tsx
@@ -243,7 +243,7 @@ const TeamSelect = () => {
               : `No teams in the database!`}
           </div>
           <Button
-            onClick={void router.push("/")}
+            onClick={() => void router.push("/")}
             variant="contained"
             endIcon={<KeyboardDoubleArrowRightIcon fontSize="small" />}
           >
